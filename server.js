6a6764bb5669adb175436236ea01160bc035eac0
jimin <aprilmin1820@gmail.com>
2020-02-15 17:20:33 +0900

    add mongoDB connect(mongoose)

--- server.js
+++ server.js
@@ -2,21 +2,26 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./routes/product');
 const orderRoutes = require('./routes/order');
 const bbsRoutes = require('./routes/bbs');
+const dbUrl = "mongodb://joke716:k9915402@ds141294.mlab.com:41294/node-rest-shop";
 
-// app.use((req, res) => {
-//    res.json({
-//        msg: '서버 시작됨!'
-//    });
-// });
+// DB connect. mongoose-> mongoDB 연결 라이브러리. 이걸통해서 데이터 오고가고.
+// mongoose.connect("mongodb+srv://jimin:1234@cluster0-gexlq.mongodb.net/test?retryWrites=true&w=majority",{})
+mongoose.connect(dbUrl,{useNewUrlParser: true,  useUnifiedTopology: true  })
+    .then(() => console.log("mongoDB connected ... "))
+    .catch(err => console.log(err.message));
 
+
+// setting
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+// route
 app.use('/product', productRoutes);
 app.use('/order', orderRoutes);
 app.use('/bbs', bbsRoutes);
